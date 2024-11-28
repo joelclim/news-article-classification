528192b5e44b58921c9ffebbb8bb7f02e9b8c238
joelclim <joel.c.lim@gmail.com>
2024-11-27 21:27:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ def get_summarizer():
 get_traditional_learning_model()
 get_deep_learning_model()
 
-# pipeline("summarization", model="google/pegasus-multi_news", device=0)
+pipeline("summarization", model="google/pegasus-multi_news")
 
 def stop_words():
     all_stopwords = stopwords.words('english')
